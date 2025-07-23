286b0f13816cac28d451549100b42feec69e3cc0
yeejuwon <157552680+yeejuwon@users.noreply.github.com>
2025-07-24 04:14:07 +0900

    import webdriver 코드 추가

--- review_analysis/crawling/aladin_crawler.py
+++ review_analysis/crawling/aladin_crawler.py
@@ -1,3 +1,4 @@
+from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.common.exceptions import NoSuchElementException, ElementNotInteractableException
