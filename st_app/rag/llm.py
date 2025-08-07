3eb6ff8adb9027d2cb936f0766f4d4da7a40fec1
Dani-dan-ee <da3967@yonsei.ac.kr>
2025-08-07 16:13:56 +0900

    Delete st_app/rag/llm.py

--- st_app/rag/llm.py
+++
@@ -1,9 +0,0 @@
-from langchain_google_genai import ChatGoogleGenerativeAI
-
-def get_llm():
-    return ChatGoogleGenerativeAI(
-        model="models/gemini-2.0-flash",
-        temperature=0.7,
-        google_api_key="-"
-    )
-
